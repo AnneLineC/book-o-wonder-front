4ba25558dcdc229353bef147e36be5137bc937ec
AnneLineC <anneline.col@gmail.com>
2021-12-03 13:48:48 +0100

    Sass installation and folders architecture creation

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import './styles.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Bienvenue sur Book'O'Wonder !
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
