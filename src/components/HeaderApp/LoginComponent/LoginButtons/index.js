4eed291f1cc0b796005899582da192cfa6976169
AnneLineC <anneline.col@gmail.com>
2021-12-05 12:09:25 +0100

    (add-loginbuttons) LoginButtons component created

---
+++ src/components/HeaderApp/LoginComponent/LoginButtons/index.js
@@ -0,0 +1,10 @@
+import './styles.scss';
+
+const LoginButtons = () => (
+  <div className="login-buttons">
+    <button type="button">Connexion</button>
+    <button type="button">Inscription</button>
+  </div>
+);
+
+export default LoginButtons;
--- src/components/HeaderApp/LoginComponent/index.js
+++ src/components/HeaderApp/LoginComponent/index.js
@@ -1,9 +1,13 @@
+import LoginButtons from './LoginButtons';
 import './styles.scss';
 
 const LoginComponent = () => (
   <div className="login-component">
-    <span class="login-component__icon"><i class="far fa-user"></i></span> 
-    <span class="login-component__text">Connexion</span>
+    <LoginButtons />
+    <div className="login-menu">
+        <span class="login-menu__icon"><i class="far fa-user"></i></span>
+        <span class="login-menu__text">Connexion</span>
+    </div>
   </div>
 );
 
