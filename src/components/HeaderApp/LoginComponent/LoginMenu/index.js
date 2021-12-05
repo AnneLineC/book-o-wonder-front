5b9c22294f7430ee321f88ab8cc7360da83ece7b
AnneLineC <anneline.col@gmail.com>
2021-12-05 12:35:05 +0100

    (modif-headerapp) Component and subcomponent responsive, and adding of the sr-only css class

--- src/components/HeaderApp/LoginComponent/LoginMenu/index.js
+++ src/components/HeaderApp/LoginComponent/LoginMenu/index.js
@@ -2,8 +2,8 @@ import './styles.scss';
 
 const LoginMenu = () => (
   <div className="login-menu">
+    <span class="login-menu__text sr-only">Connexion</span>
     <span class="login-menu__icon"><i class="far fa-user"></i></span>
-    <span class="login-menu__text">Connexion</span>
   </div>
 );
 
