42e888db58bb3c567a5ca1321f24eb8988d2fb51
AnneLineC <anneline.col@gmail.com>
2021-12-07 10:54:10 +0100

    (add-eslint) Eslint config airbnb installed

--- src/components/HeaderApp/index.js
+++ src/components/HeaderApp/index.js
@@ -3,24 +3,24 @@ import LoginComponent from './LoginComponent';
 import './styles.scss';
 
 const HeaderApp = () => {
-    const isLogged = false;
-    return (
-        <header className="header-app">
-            <div className="header-app__element header-app__element--menu">
-                {!isLogged && <LoginComponent />}
-                {isLogged && <LoggedMenu />}
-            </div>
-            <button type="button" className="header-app__element header-app__element--categories">
-                <span className="sr-only">Catégories</span>
-                <i className="header-app__icon fas fa-book" />
-            </button>
-            <button type="button" className="header-app__element header-app__element--audio-player">
-                <span className="sr-only">Lecteur audio</span>
-                <i className="header-app__icon fas fa-headphones" />
-            </button>
-            <h1 className="header-app__title">Book'O'Wonder</h1>
-        </header>
-    );
-}
+  const isLogged = false;
+  return (
+    <header className="header-app">
+      <div className="header-app__element header-app__element--menu">
+        {!isLogged && <LoginComponent />}
+        {isLogged && <LoggedMenu />}
+      </div>
+      <button type="button" className="header-app__element header-app__element--categories">
+        <span className="sr-only">Catégories</span>
+        <i className="header-app__icon fas fa-book" />
+      </button>
+      <button type="button" className="header-app__element header-app__element--audio-player">
+        <span className="sr-only">Lecteur audio</span>
+        <i className="header-app__icon fas fa-headphones" />
+      </button>
+      <h1 className="header-app__title">Book'O'Wonder</h1>
+    </header>
+  );
+};
 
 export default HeaderApp;
