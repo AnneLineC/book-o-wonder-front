8861082a3009b7f4578e237c62ced26666ba2e95
AnneLineC <anneline.col@gmail.com>
2021-12-09 14:58:43 +0100

    (modify-registerapp) User token and datas setted into the state when logged

--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
-import { LOGIN_ATTEMPT, setCurrentUser, REGISTER_ATTEMPT, LOAD_CATEGORIES_FROM_API, setCategories } from '../actions';
+import {
+  LOGIN_ATTEMPT,
+  setCurrentUserJWT,
+  REGISTER_ATTEMPT,
+  LOAD_CATEGORIES_FROM_API,
+  setCategories,
+  setCurrentUserData,
+} from '../actions';
 
 const baseURI = 'http://52.87.193.62';
 
@@ -8,10 +15,7 @@ const apiMiddleWare = (store) => (next) => (action) => {
   switch (action.type) {
     case LOGIN_ATTEMPT: {
       const { emailValue, passwordValue } = store.getState().user;
-      console.log('test');
-      console.log(emailValue, passwordValue);
 
-      // api's url so that we can connect back and front together
       axios.post(`${baseURI}/api/login_check`, {
         username: emailValue,
         password: passwordValue,
@@ -19,8 +23,22 @@ const apiMiddleWare = (store) => (next) => (action) => {
         (response) => {
           console.log(response.data);
 
-          // dispatch to log the user
-          // store.dispatch(setCurrentUser(response.data));
+          // dispatch to get the user's token
+          store.dispatch(setCurrentUserJWT(response.data.token));
+
+          // we save the token into axios instance
+          axios.defaults.headers.common.Authorization = `Bearer ${response.data.token}`;
+
+          axios.get(
+            `${baseURI}/api/v1/user/user`,
+          ).then(
+            (result) => {
+              console.log(result);
+              store.dispatch(setCurrentUserData(result.data));
+            },
+          ).catch(
+            (error) => console.log(error),
+          );
         },
       ).catch(
         (error) => console.log(error),
@@ -29,6 +47,7 @@ const apiMiddleWare = (store) => (next) => (action) => {
       next(action);
       break;
     }
+
     case REGISTER_ATTEMPT: {
       const {
         nicknameValue,
@@ -36,8 +55,6 @@ const apiMiddleWare = (store) => (next) => (action) => {
         passwordValue,
         // passwordConfirmValue,
       } = store.getState().user;
-
-      console.log(nicknameValue, emailValue, passwordValue);
 
       // api's url so that we can connect back and front together
       axios.post(`${baseURI}/api/v1/register`, {
@@ -59,6 +76,7 @@ const apiMiddleWare = (store) => (next) => (action) => {
       next(action);
       break;
     }
+
     case LOAD_CATEGORIES_FROM_API: {
       axios.get(`${baseURI}/api/v1/category`).then(
         (response) => {
@@ -68,9 +86,12 @@ const apiMiddleWare = (store) => (next) => (action) => {
       ).catch(
         (error) => console.log(error.toJSON()),
       );
+      next(action);
+      break;
     }
+
     default:
-    next(action);
+      next(action);
   }
-}
+};
 export default apiMiddleWare;
