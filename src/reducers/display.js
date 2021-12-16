b316a57f932834c54fa0f7e7b7ee690215926a76
BastienAut <deh2win@gmail.com>
2021-12-16 11:30:13 +0100

    (update-accountpage) fixed display reducer

--- src/reducers/display.js
+++ src/reducers/display.js
@@ -1,15 +1,8 @@
 import { SET_CATEGORIES, SET_DISPLAY, SET_MEDIA_PLAYER_DISPLAY } from '../actions';
 
 export const initialState = {
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-  baseURI: 'http://3.83.176.59',
-=======
-  baseURI: 'http://52.207.253.181',
->>>>>>> Stashed changes
-=======
-  baseURI: 'http://3.83.127.40',
->>>>>>> development
+
+  baseURI: 'http://50.16.130.142',
   categoriesList: [],
   connexionMenu: false,
   connectedMenu: false,
