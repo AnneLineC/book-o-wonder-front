76ab52ab164ac5f3a685f5dc7ec6b930cd397969
AnneLineC <anneline.col@gmail.com>
2021-12-17 18:13:56 +0100

    (modify-homepage) Pinnedpaged books available on homepage when logged

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,56 +1,87 @@
 import { Carousel } from 'react-responsive-carousel';
-import './styles.scss';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 // eslint-disable-next-line no-unused-vars
 import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 // eslint-disable-next-line no-unused-vars
 import ReactDOM from 'react-dom';
+import BookCard from '../BookCard';
+import './styles.scss';
 
-const HomePage = () => (
-  <div className="home-page-main">
+const HomePage = () => {
+  const isLogged = useSelector((state) => state.user.logged);
+  const nickname = useSelector((state) => state.user.name);
+  const pinnedpages = useSelector((state) => state.user.pinnedpages);
+  const baseURI = useSelector((state) => (state.display.baseURI));
 
-    <p className="home-page__title">Book'O'Wonder, c'est quoi ?</p>
+  return (
+    <div className="home-page-main">
 
-    <div className="home-page__container">
+      {isLogged
+      && (
+        <>
+          <p className="home-page__title">Bienvenue {nickname} !</p>
+          <p className="home-page__pinnedpaged-title">Reprendre une lecture en cours</p>
+          <div className="pinnedpaged-books">
+            {pinnedpages.map((pinnedpage) => (
+              <Link key={pinnedpage.book.id} to={`/livre/${pinnedpage.book.id}/lecture`}>
+                <div className="pinnedpaged-books__card">
+                  {/* <BookCard picture={pinnedpage.book.picture} /> */}
+                  <div
+                    className="bookcard"
+                    style={{ backgroundImage: `url("${baseURI}/images_book_folder/${pinnedpage.book.picture}")` }}
+                  />
+                </div>
+              </Link>
+            ))}
+          </div>
+        </>
+      )}
 
-      <div className="home-page__highlight">
-        <i className="home-page__avatar fas fa-book" />
+      {!isLogged
+      && (
+      <>
+        <p className="home-page__title">Book'O'Wonder, c'est quoi ?</p>
+        <div className="home-page__container">
+          <div className="home-page__highlight">
+            <i className="home-page__avatar fas fa-book" />
+            <p className="home-page__content">
+              {/* eslint-disable-next-line max-len */}
+              Un lecteur de livre intéractif vous permettant de vivre une aventure parmi une ludothèque fournie
+            </p>
+          </div>
+          <div className="home-page__highlight">
+            <i className="home-page__avatar fas fa-headphones" />
+            <p className="home-page__content">
+              {/* eslint-disable-next-line max-len */}
+              Un lecteur audio vous permettant mettre une ambiance sonore afin de savourer votre lecture
+            </p>
+          </div>
+          <div className="home-page__highlight">
+            <i className="home-page__avatar fas fa-bookmark" />
+            <p className="home-page__content">
+              Un système de marque-page vous permettant de reprendre votre dernière lecture en cours
+            </p>
+          </div>
+        </div>
+      </>
+      )}
 
-        <p className="home-page__content">
-          {/* eslint-disable-next-line max-len */}
-          Un lecteur de livre intéractif vous permettant de vivre une aventure parmi une ludothèque fournie
-        </p>
-      </div>
+      <Carousel infiniteLoop useKeyboardArrows autoPlay autoFocus>
+        <div className="home-page__test">
+          <img src="https://i.postimg.cc/BbpCMQ48/alice.jpg" alt="alice" border="0" />
+        </div>
 
-      <div className="home-page__highlight">
-        <i className="home-page__avatar fas fa-headphones" />
+        <div className="home-page__test">
+          <img src="https://i.postimg.cc/g07PQbSc/alacrois-edesmondes.jpg" alt="croiseedesmondes" border="0" />
+        </div>
+        <div className="home-page__test">
+          <img src="https://i.postimg.cc/z3YBgpxn/template.jpg" alt="template" border="0" />
+        </div>
+      </Carousel>
 
-        <p className="home-page__content">
-          Un lecteur audio vous permettant mettre une ambiance sonore afin de savourer votre lecture
-        </p>
-      </div>
-
-      <div className="home-page__highlight">
-        <i className="home-page__avatar fas fa-bookmark" />
-        <p className="home-page__content">
-          Un système de marque-page vous permettant de reprendre votre dernière lecture en cours
-        </p>
-      </div>
     </div>
-
-    <Carousel infiniteLoop useKeyboardArrows autoPlay autoFocus>
-      <div className="home-page__test">
-        <img src="https://i.postimg.cc/BbpCMQ48/alice.jpg" alt="alice" border="0" />
-      </div>
-
-      <div className="home-page__test">
-        <img src="https://i.postimg.cc/g07PQbSc/alacrois-edesmondes.jpg" alt="croiseedesmondes" border="0" />
-      </div>
-      <div className="home-page__test">
-        <img src="https://i.postimg.cc/z3YBgpxn/template.jpg" alt="template" border="0" />
-      </div>
-    </Carousel>
-
-  </div>
-);
+  );
+};
 
 export default HomePage;
